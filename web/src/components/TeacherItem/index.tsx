76657cc247c81f87c4cc04214bda40dbd754b114
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2021-02-17 19:49:54 -0300

    style: componentizing teacher item

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { SiWhatsapp } from 'react-icons/si';
+import { Container } from './styles';
+
+interface Props {
+  avatar: string;
+  bio: string;
+  cost: string;
+  name: string;
+  subject: string;
+}
+
+const TeacherItem: React.FC<Props> = ({ avatar, bio, cost, name, subject }) => {
+  return (
+    <Container>
+      <header>
+        <img src={avatar} alt={name} />
+        <div>
+          <strong>{name}</strong>
+          <span>{subject}</span>
+        </div>
+      </header>
+      <p>{bio}</p>
+      <footer>
+        <p>
+          Preço/hora<strong>R$ {cost}</strong>
+        </p>
+        <button type="button">
+          <SiWhatsapp />
+          Entrar em contato
+        </button>
+      </footer>
+    </Container>
+  );
+};
+
+export default TeacherItem;
